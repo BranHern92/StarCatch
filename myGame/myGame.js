b55c3b56f372068d263e9c319788bc12b9d1c2a2
Brandon Hernandez <bhernandez.de@mysummitps.org>
2016-10-17 17:37:02 +0000

    update

--- myGame/myGame.js
+++ myGame/myGame.js
@@ -48,7 +48,7 @@ function create() {
     
     game.physics.arcade.enable(player);
     player.body.bounce.y = 0;
-    player.body.gravity.y = 275;
+    player.body.gravity.y = 250;
     player.body.collideWorldBounds = true;
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
